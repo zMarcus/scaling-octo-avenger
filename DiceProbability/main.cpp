0baa1a3c66935a714a9b5dd71e2d144f9e3f5b4d
Marcus Omaiye <zxmarcus@gmail.com>
2014-05-28 20:22:58 -0700

    Added the probability calculation
    Set as a separate method, might want to add formatting in the next revision! :D

--- DiceProbability/main.cpp
+++ DiceProbability/main.cpp
@@ -14,53 +14,109 @@ using namespace std;
 
 int twoCount, threeCount, fourCount, fiveCount, sixCount,
     sevenCount, eightCount, nineCount, tenCount, elevenCount,
-    twelveCount;
+    twelveCount = 0;
 int ROLLS = 10;
+float prb = 0;
 
 
 void recordDice(int diceTotal){
     switch (diceTotal) {
         case 2:
+			cout << "increment 2!" << endl;
             twoCount++;
             break;
         case 3:
             threeCount++;
+			break;
         case 4:
             fourCount++;
+			break;
         case 5:
             fiveCount++;
+			break;
         case 6:
             sixCount++;
+			break;
         case 7:
             sevenCount++;
             break;
         case 8:
             eightCount++;
+			break;
         case 9:
             nineCount++;
+			break;
         case 10:
             tenCount++;
+			break;
         case 11:
+			cout << "increment 11!" << endl;
             elevenCount++;
+			break;
         case 12:
             twelveCount++;
+			break;
         default:
             break;
     }
 }
 
+double prbCalc(int numberCount){
+	prb = numberCount / (double) ROLLS;
+	prb *= 100;
+	return prb;
+}
+
 void showResults(){
-    cout << "Resulted in 2 " << twoCount << " times" << endl;
-    cout << "Resulted in 3 " << threeCount << " times" << endl;
-    cout << "Resulted in 4 " << fourCount << " times" << endl;
-    cout << "Resulted in 5 " << fiveCount << " times" << endl;
-    cout << "Resulted in 6 " << sixCount << " times" << endl;
-    cout << "Resulted in 7 " << sevenCount << " times" << endl;
-    cout << "Resulted in 8 " << eightCount << " times" << endl;
-    cout << "Resulted in 9 " << nineCount << " times" << endl;
-    cout << "Resulted in 10 " << tenCount << " times" << endl;
-    cout << "Resulted in 11 " << elevenCount << " times" << endl;
-    cout << "Resulted in 12 " << twelveCount << " times" << endl;
+    //cout << "Resulted in 2 " << twoCount << " times" << endl;
+	//cout << "Resulted in 3 " << threeCount << " times" << endl;
+    //cout << "Resulted in 4 " << fourCount << " times" << endl;
+    //cout << "Resulted in 5 " << fiveCount << " times" << endl;
+    //cout << "Resulted in 6 " << sixCount << " times" << endl;
+    //cout << "Resulted in 7 " << sevenCount << " times" << endl;
+    //cout << "Resulted in 8 " << eightCount << " times" << endl;
+    //cout << "Resulted in 9 " << nineCount << " times" << endl;
+    //cout << "Resulted in 10 " << tenCount << " times" << endl;
+    //cout << "Resulted in 11 " << elevenCount << " times" << endl;
+	//cout << "Resulted in 12 " << twelveCount << " times" << endl;
+
+
+	prbCalc(twoCount);
+	printf("Resulted in 2 %d times -- %0.1f%%\n", twoCount, prb);
+	//prb = twoCount / (double) ROLLS;
+	//prb *= 10;
+
+	prbCalc(threeCount);
+	printf("Resulted in 3 %d times -- %0.1f%%\n", threeCount, prb);
+
+	prbCalc(fourCount);
+	printf("Resulted in 4 %d times -- %0.1f%%\n", fourCount, prb);
+
+	prbCalc(fiveCount);
+	printf("Resulted in 5 %d times -- %0.1f%%\n", fiveCount, prb);
+
+    prbCalc(sixCount);
+	printf("Resulted in 6 %d times -- %0.1f%%\n", sixCount, prb);
+
+	prbCalc(sevenCount);
+	printf("Resulted in 7 %d times -- %0.1f%%\n", sevenCount, prb);
+
+	prbCalc(eightCount);
+	printf("Resulted in 8 %d times -- %0.1f%%\n", eightCount, prb);
+
+	prbCalc(nineCount);
+	printf("Resulted in 9 %d times -- %0.1f%%\n", nineCount, prb);
+
+	prbCalc(tenCount);
+	printf("Resulted in 10 %d times -- %0.1f%%\n", tenCount, prb);
+
+	prbCalc(elevenCount);
+	printf("Resulted in 11 %d times -- %0.1f%%\n", elevenCount, prb);
+
+	prbCalc(twelveCount);
+	printf("Resulted in 11 %d times -- %0.1f%%\n", twelveCount, prb);
+	
+    
 }
 
 int main(int argc, const char * argv[])
@@ -94,5 +150,6 @@ int main(int argc, const char * argv[])
     
     showResults();
     
+	system("pause");
     return 0;
 }
